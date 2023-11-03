336f64bf7266fe404ef535da93f58972509a8cc9
angelDariaux <angeldariaux@gmail.com>
2023-11-03 15:52:31 -0600

    Agregado node_modules al .gitignore

---
+++ README.md
@@ -0,0 +1,10 @@
+Please make sure to install these dependencies by running
+npm init
+npm i puppeteer jest 
+npm i -D prettier 
+npm i jest-image-snapshot 
+npm i @axe-core/puppeteer
+
+
+To run a script, navigate to the project directory in your terminal and run npm run <script-name.test.js>. 
+For example, to run the Google tests script, you would run npm run google-tests.test.js.
---
+++ __test__/casoDePruebas1.test.js
@@ -0,0 +1,42 @@
+const puppeteer = require('puppeteer');
+const {click, type, getText} = require('../libs/helpers')
+const timeDelay = 3_600  // 1 hora de espera maxima (caso extremo de testeo)
+let browser 
+let page 
+
+describe('Automatizacion del formulario de DemoQa', () => { 
+  // Hook beforeAll
+  beforeAll ( async () => { 
+    browser = await puppeteer.launch({ 
+      headless: false,  // Nos permite ver lo que pasa mientras el robot testea
+      defaultViewport: null, // Set default viewport to null
+      args: ['--start-maximized'] // inicia el navegador para que use toda la pantalla
+    });
+
+    // Dandole el valor a page, con la nueva instancia de pagina creada.
+    page = await browser.newPage()
+
+    await page.goto('https://demoqa.com/automation-practice-form', { waituntil: 'networkidel0' })  // pasandole la URL a testear y debe esperar a que todo cargue para poder testear.
+  }, timeDelay);
+
+  // Hook afterAll 
+  afterAll ( async () => { 
+    // espera 3 segundos antes que cierre el navegador, despues de haber hecho el test.
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    await browser.close()
+  }, timeDelay);   
+
+  
+  // Ejecutando los casos de prueba. 
+  it('Caso de uso 1, Happy path', async () => { 
+    
+    // Dandole un delay al type para poder visualizarlo
+    let typeDelay = {delay: 100}
+
+    await page.waitForSelector('#submit')
+
+    await type(page, '#firstName', 'Angel', typeDelay)
+    await type(page, '#lastName', 'Hackerman', typeDelay)
+    await type(page, '#userEmail', 'AngelHackerman@emailsecreto.com', typeDelay)
+  })
+})
